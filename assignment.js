739408c8c521e395b72d1cf677478c71122e040f
Mashhad Ali Shah <165510733+MASHHADALI@users.noreply.github.com>
2024-09-20 00:22:22 +0500

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,68 @@
+
+
+// TASK#1
+// let num1 = 23.499999
+// let num2 = 93.33323234352523
+// let num3 = 78.9433
+// let num4 = 99.9999
+// let num5 = 34.1123
+
+
+// alert(Math.round(num1))
+// alert(Math.round(num2))
+// alert(Math.round(num3))
+// alert(Math.round(num4))
+// alert(Math.round(num5))
+
+
+
+
+// TASK#2
+
+// alert(Math.random()*15)
+
+// let num = (Math.random(1)*20)
+// alert(Math.round(num))
+
+// let x = 60
+// alert(Math.random()*x)
+
+// alert(Math.random()/2)
+
+// let x = (Math.random())
+// alert(x)
+
+
+
+
+
+
+// TASK#3
+// let a = ("123")
+// let b = ("45.67")
+// let c = ("89.01")
+// let d = ("100") 
+
+// alert(parseFloat(a))
+// alert(parseFloat(b))
+// alert(parseFloat(c))
+// alert(parseFloat(d))
+
+// alert(Number(a))
+// alert(Number(b))
+// alert(Number(c))
+// alert(Number(d))
+
+
+
+
+
+// TASK#4
+// let x= (Math.random ()*41) + 10
+// alert(Math.floor(x))
+
+
+
+// // TASK#5
+// let num = ("300")
+// alert(Number(num))
